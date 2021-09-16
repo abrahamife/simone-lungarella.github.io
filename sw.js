9bf551003eeaad755c76916373215def36b921ea
Simone <simone.l1996@hotmail.it>
2021-09-16 14:22:30 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d60b746590338880f884.js"
+    "url": "webpack-runtime-5279ee9f1631c2d3806c.js"
   },
   {
     "url": "framework-aa35e29fbb74d6100667.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5c7f7f463e13e64488d37d56c5189624"
+    "revision": "3b3100fb7e0768cee9c5e31c204084e5"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-50fcc746a3b9bd6c1224.js"
